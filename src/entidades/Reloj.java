f16a4aeef0da9778d3cf3f3d82b828e186311724
Julio Flores <juliofloresth@gmail.com>
2023-08-17 00:13:42 -0300

    Modificacion de Reloj

---
+++ src/entidades/Reloj.java
@@ -0,0 +1,42 @@
+package entidades;
+
+public class Reloj {
+
+    protected String dia,hora,modelo; 
+    protected  long numSerie;
+
+    public Reloj(String dia, String hora, String modelo, long numSerie) {
+        this.dia=dia;
+        this.hora=hora;
+        this.modelo=modelo;
+        this.numSerie=numSerie;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void incrementarDia() {
+        /*este metodo incrementa el dia por ejemplo, lunes a martes.
+        Usando LocalDate*/
+    }
+
+    public void incrementarHora() {
+        /*este metodo incrementa la hora
+         usando LocalDate, parsear hora de string a LocalDate*/
+    }
+
+    public void limpiarPantalla() {
+        //consultar que hace este metodo.
+    }
+
+    @Override
+    public String toString() {
+        return "Reloj{" + "dia=" + dia + ", hora=" + hora + ", modelo=" + modelo + ", numSerie=" + numSerie + '}';
+    }
+    
+}
