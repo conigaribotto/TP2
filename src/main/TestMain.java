a31e959cee0ad234b3411bc1b025dfff82d852da
Scott505 <142347083+Scott505@users.noreply.github.com>
2023-08-17 16:38:17 -0300

    Probando lo del Main

--- src/main/TestMain.java
+++ src/main/TestMain.java
@@ -10,7 +10,7 @@ public class TestMain {
    
     public static void main(String[] args) {
         
-        //4_ Desde la java main class, llamar a los metodos de unReloj, accediendo desde objeto unaPersona
+        //4_ Desde la java main class, llamar a los metodos de un Reloj, accediendo desde objeto una Persona
         // Llamar los metodos de todas las clasess
          
         RelojFit relojFit = new RelojFit("Martes", "19:00hs", "Modelo Fit", 123456,2,2);   
@@ -18,7 +18,7 @@ public class TestMain {
         Persona persona = new Persona("Coni", "Genia Garibotto", 23, 1.59, relojFit);
 
         // Llamar a los métodos de la clase RelojFit a través del objeto Persona
-        System.out.println("Coordenadas del reloj Fit: x=" + persona.getReloj().getX() + ", y=" + persona.getReloj().getY());
+        System.out.println("Coordenadas del reloj Fit: x=" + relojFit.getX() + ", y=" + relojFit.getY());
 
         System.out.println("Día del reloj: " + relojFit.getDia());
         System.out.println("Hora del reloj: " + relojFit.getHora());
