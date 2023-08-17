6d01d7da4784609cf07b4be1b8fd1c22f31da30e
Julio Flores <juliofloresth@gmail.com>
2023-08-17 00:13:59 -0300

    Modificacion de Main

---
+++ src/main/TestMain.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Usuario
+ */
+public class TestMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //4_ Desde la java main class, llamar a los metodos de unReloj, accediendo desde objeto unaPersona
+        // Llamar los metodos de todas las clasess
+
+    }
+    
+}
